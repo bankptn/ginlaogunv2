5602a34fb5aa2ef57a068546273dcc60d1d128cd
bankptn <bank19979@gmail.com>
2020-12-13 09:06:47 +0700

    Solve api reservation

--- ginloagun/src/service/api_reservation.js
+++ ginloagun/src/service/api_reservation.js
@@ -1,5 +1,5 @@
 import httpClient from "@/service/httpClient";
-import { server } from "@/service/constants";
+import { server, apiUrl } from "@/service/constants";
 
 export const createReservation = async (values) => {
     console.log(values)
@@ -10,5 +10,10 @@ export const createReservation = async (values) => {
     bodyFormData.append("tableAmount", values.tableAmount);
     bodyFormData.append("detail", values.detail);
     var result = await httpClient.post(server.RESERVATION, bodyFormData);
+    return result
+};
+
+export const getRemainTableByrid = async (rid) => {
+    var result = await httpClient.get(apiUrl+ "/" + "getRemainTable" + "/" + rid);
     return result
 };
